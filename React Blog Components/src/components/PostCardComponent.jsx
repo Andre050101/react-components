41b272f6f81431fa5f5acc8a05bf08043e944336
Andre050101 <agiancristiano90@gmail.com>
2024-12-12 15:43:59 +0100

    PostCardComponent structure done

---
+++ React Blog Components/src/components/PostCardComponent.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function PostCardComponent() {
+    return (
+        <div>
+            <div>
+                <span>600 x 400</span>
+            </div>
+            <div>
+                <h2>Titolo del post</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta minima consequatur odit voluptate cupiditate nesciunt. Molestiae, sequi ipsa? Sint enim officiis delectus porro corporis!</p>
+                <button>Leggi di più</button>
+            </div>
+        </div>
+    );
+}
+
+export default PostCardComponent;
