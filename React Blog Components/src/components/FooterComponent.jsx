9f5672c615503361e5048281e37b67b32062cb12
Andre050101 <agiancristiano90@gmail.com>
2024-12-12 15:45:11 +0100

    FooterComponent structure done

--- React Blog Components/src/components/FooterComponent.jsx
+++ React Blog Components/src/components/FooterComponent.jsx
@@ -1,1 +1,9 @@
 import React from "react";
+
+function FooterComponent() {
+    return (
+        <footer>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, dolorem expedita!</p>
+        </footer>
+    );
+}
